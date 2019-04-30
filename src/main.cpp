98a6383060060bed1aa7d1870867d6ae8f63ebf8
Ellen <ellennugroho9@gmail.com>
2019-04-30 13:24:24 +0700

    add hoodjaaaaaaaaaaaaaaaaaaaan

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,142 @@
 #include <GL/glu.h>
 #include <iostream>
 
+#define MAX_PARTICLES 1000
+#define WCX		640
+#define WCY		480
+#define RAIN	0
+#define SNOW	1
+#define	HAIL	2
+
 using namespace std;
+
+// Rain
+float slowdown = 2.0;
+float velocity = 0.0;
+float zoom = -40.0;
+float pan = 0.0;
+float tilt = 0.0;
+float hailsize = 0.1;
+
+int loop;
+int fall;
+
+//floor colors
+float r = 0.0;
+float g = 1.0;
+float b = 0.0;
+float ground_points[21][21][3];
+float ground_colors[21][21][4];
+float accum = -10.0;
+
+typedef struct {
+  // Life
+  bool alive;	// is the particle alive?
+  float life;	// particle lifespan
+  float fade; // decay
+  // color
+  float red;
+  float green;
+  float blue;
+  // Position/direction
+  float xpos;
+  float ypos;
+  float zpos;
+  // Velocity/Direction, only goes down in y dir
+  float vel;
+  // Gravity
+  float gravity;
+}particles;
+
+// Paticle System
+particles par_sys[MAX_PARTICLES];
+
+void normal_keys(unsigned char key, int x, int y) {
+  if (key == 'r') { // Rain
+    fall = RAIN;
+    glutPostRedisplay();
+  }
+  if (key == 'h') { // Hail
+    fall = HAIL;
+    glutPostRedisplay();
+  }
+  if (key == 's') { // Snow
+    fall = SNOW;
+    glutPostRedisplay();
+  }
+  if (key == '=') { //really '+' - make hail bigger
+    hailsize += 0.01;
+  }
+  if (key == '-') { // make hail smaller
+    if (hailsize > 0.1) hailsize -= 0.01;
+  }
+  if (key == ',') { // really '<' slow down
+    if (slowdown > 4.0) slowdown += 0.01;
+  }
+  if (key == '.') { // really '>' speed up
+    if (slowdown > 1.0) slowdown -= 0.01;
+  }
+  if (key == 'q') { // QUIT
+    exit(0);
+  }
+}
+
+// Initialize/Reset Particles - give them their attributes
+void initParticles(int i) {
+    par_sys[i].alive = true;
+    par_sys[i].life = 1.0;
+    par_sys[i].fade = float(rand()%100)/1000.0f+0.003f;
+
+    par_sys[i].xpos = (float) (rand() % 21) - 10;
+    par_sys[i].ypos = 15.0;
+    par_sys[i].zpos = (float) (rand() % 21) - 10;
+
+    par_sys[i].red = 0.5;
+    par_sys[i].green = 0.5;
+    par_sys[i].blue = 1.0;
+
+    par_sys[i].vel = velocity;
+    par_sys[i].gravity = -0.8;//-0.8;
+
+}
+
+// For Rain
+void drawRain() {
+  float x, y, z;
+  for (loop = 0; loop < MAX_PARTICLES; loop=loop+2) {
+    if (par_sys[loop].alive == true) {
+      x = par_sys[loop].xpos;
+      y = par_sys[loop].ypos;
+      z = par_sys[loop].zpos;
+
+      // Draw particles
+      glColor3f(0.5, 0.5, 1.0);
+      glBegin(GL_LINES);
+        glVertex3f(x, y, z);
+        glVertex3f(x, y+0.5, z);
+      glEnd();
+
+      // Update values
+      //Move
+      // Adjust slowdown for speed!
+      par_sys[loop].ypos += par_sys[loop].vel / (slowdown*1000);
+      par_sys[loop].vel += par_sys[loop].gravity;
+      // Decay
+      par_sys[loop].life -= par_sys[loop].fade;
+
+      if (par_sys[loop].ypos <= -10) {
+        par_sys[loop].life = -1.0;
+      }
+      //Revive
+      if (par_sys[loop].life < 0.0) {
+        initParticles(loop);
+      }
+    }
+  }
+}
+
+
+// Rain end
 
 GLfloat xRotated, yRotated, zRotated;
 GLfloat scale;
@@ -84,6 +219,8 @@ void display(void) {
     glClear(GL_COLOR_BUFFER_BIT);
     // clear the identity matrix.
     glLoadIdentity();
+
+
     // rotation about X axis
     glRotatef(xRotated,1.0,0.0,0.0);
     // rotation about Y axis
@@ -95,10 +232,13 @@ void display(void) {
     // glScalef(1.25,1.0,1.0);
     glTranslatef(-0.5,0,0);
     glutSolidCube(size);
-
+    
     glTranslatef(0.7,-0.25,0.0);
     glScalef(1.0/1.25,1.0,2.0);
     glutSolidCube(0.5);
+
+    drawRain();
+    glPopMatrix();
 
     GLUquadricObj *disk;
     disk = gluNewQuadric();
@@ -144,7 +284,10 @@ void display(void) {
     gluDisk(disk, 0, 0.15, 32, 32);
     glTranslatef(0,0,0.1);
     gluDisk(disk, 0, 0.15, 32, 32);
-    glTranslatef(0,0,-0.1);
+
+    // glTranslatef(40,0,10);
+
+    
 
     glFlush();
     glutSwapBuffers();
@@ -165,6 +308,9 @@ void init(void) {
     glEnable(GL_DEPTH_TEST);
     make_tex();
     glEnable(GL_TEXTURE_2D);
+    for (loop = 0; loop < MAX_PARTICLES; loop++) {
+        initParticles(loop);
+    }
 }
 
 void keyPressed (unsigned char key, int x, int y) {  
